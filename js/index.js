09aa3f3e810a5075f9e2df96cf8354266e48eb08
Sergey Volkov <serzhvolkov03@gmail.com>
2023-04-28 15:26:34 +0300

    feat: add js

---
+++ js/index.js
@@ -0,0 +1,18 @@
+// document.addEventListener("keydown", (event) => {
+//   console.log(event.key, event.code);
+// });
+
+// const textArea = document.querySelector(".textarea");
+// textArea.value = "ghadfva";
+
+// const keys = {
+//   Backquote: {
+//     line: 1,
+//     en: "`",
+//     ru: "ё",
+//     enShift: "~",
+//     ruShift: "Ё",
+//     additionClasses: ["black", "space"],
+//     shiftable: true,
+//   },
+// };
